9d1fdc4b1b0799f8f09c4457fb2855cd72ba0c2b
Nano <Nano.nzt@gmail.com>
2018-05-12 13:34:13 -0500

    Added Home, Detail, Create, and minor bug fixs

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return (self.body[:101] + '...')
+
+    def pubdate_pretty(self):
+        return self.pubdate.strftime('%b %e %Y')
